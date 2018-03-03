e8b5609b2cd4fe7929b2593dd44790ab3964d893
momokata <huijingdandan@gmail.com>
2018-03-03 20:51:52 +0800

    no message

--- 3.6/config/logininfo.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'username' => 'admin',
-  'passwd' => '21232f297a57a5a743894a0e4a801fc3',
-);
--- 3.6/config/playerconfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'm3u8' => 'http://jiexi.rxffzh.cn/mp4.php?url=',
-  'mp4' => 'http://jiexi.rxffzh.cn/mp4.php?url=',
-);
