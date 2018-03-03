554f642fcb55cfc0e342adc9127cfc936bf013f3
momokata <huijingdandan@gmail.com>
2018-03-03 20:50:45 +0800

    no message

---
+++ 3.6/resources/views/user/userregister.blade.php
@@ -0,0 +1,52 @@
+<html><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="keywords" content="Internship Sign In &amp; Sign Up Form Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements">
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- Custom Theme files -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+    <link href="css/snow.css" rel="stylesheet" type="text/css" media="all">
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all">
+    <!-- //Custom Theme files -->
+    <!-- web font -->
+
+    <!-- //web font -->
+    <style>html, * {-webkit-user-select:text!important; -moz-user-select:text!important;}</style></head>
+<body>
+<div class="snow-container">
+    <div class="snow foreground"></div>
+    <div class="snow foreground layered"></div>
+    <div class="snow middleground"></div>
+    <div class="snow middleground layered"></div>
+    <div class="snow background"></div>
+    <div class="snow background layered"></div>
+</div>
+
+<div class="top-buttons-agileinfo">
+    <a href="index.html">登录</a><a href="signup.html" class="active">免费注册</a>
+</div>
+<h1>登录和免费注册页面</h1>
+<div class="main-agileits">
+    <!--form-stars-here-->
+    <div class="form-w3-agile">
+        <h2 class="sub-agileits-w3layouts">免费注册</h2>
+        <form action="#" method="post">
+            <input type="text" name="Username" placeholder="用户名" required="">
+            <input type="password" name="Password" placeholder="密码" required="">
+            <input type="password" name="Password" placeholder="确认密码" required="">
+            <div class="submit-w3l">
+                <input type="submit" value="免费注册">
+            </div>
+        </form>
+    </div>
+</div>
+<!--//form-ends-here-->
+<!-- copyright -->
+<div class="copyright w3-agile">
+    <p> Copyright © <a href="http://www.haook.cn" title="HTML模板网">www.haook.cn</a></p>
+</div>
+<!-- //copyright -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+
+
+</body></html>
